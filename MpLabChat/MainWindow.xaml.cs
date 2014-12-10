4aac021b358a6be23de4d3937fc264e19193e3d8
Kapitoshka <serezgik_04@mail.ru>
2014-12-10 18:33:27 +0500

    Оболочка уже кусками работает
    При отсутствии ответа от сервера - вылетает. Оболочка: многие вещи
    работают, остались мелочи.

--- MpLabChat/MainWindow.xaml.cs
+++ MpLabChat/MainWindow.xaml.cs
@@ -13,6 +13,53 @@ namespace MpLabChat
     public MainWindow()
     {
       InitializeComponent();
+      var a = new DispatcherTimer { Interval = TimeSpan.FromSeconds(2) };
+      a.Tick += delegate
+      {
+        var chatClient = new ChatServiceClient();
+        var LiN = false;
+
+        var NAME = (names.SelectedIndex != -1 && names.SelectedIndex < names.Items.Count)
+          ? (string)((ListBoxItem)names.Items[names.SelectedIndex]).Content
+          : "";
+
+        logins.Items.Clear();
+        names.Items.Clear();
+
+        var l = new string[1];
+        var n = new string[1];
+        chatClient.Spiski(ref l, ref n);
+        foreach (var i in l) logins.Items.Add(new ListBoxItem { Content = i });
+        foreach (var i in n) names.Items.Add(new ListBoxItem { Content = i });
+
+        var obn = false;
+        if (NAME != "" && n.Contains(NAME))
+        {
+          var num = -1;
+          n.First(i =>
+          {
+            num++;
+            return i == NAME;
+          });
+          names.SelectedIndex = num;
+        }
+        if (names.SelectedIndex != -1 && names.SelectedIndex < names.Items.Count)
+        {
+          foreach (
+            var i in
+              chatClient.ClickSpisok((string)((ListBoxItem)names.Items[names.SelectedIndex]).Content,
+                pole.Text.Split('\n').Count()))
+            pole.Text += i + "\n";
+          obn = true;
+        }
+        pole.IsEnabled = vvod.IsEnabled = enter.IsEnabled = obn;
+
+      };
+      a.Start();
+      Closing += delegate
+      {
+        loginExit_Click(null, null);
+      };
     }
     private void loginE_Click(object sender, RoutedEventArgs e)
     {
@@ -28,7 +75,7 @@ namespace MpLabChat
       loginExit.Visibility = Visibility.Visible;
 
       logins.IsEnabled = true;
-      os.Visibility = Visibility.Hidden;
+      
     }
     private void loginExit_Click(object sender, RoutedEventArgs e)
     {
@@ -39,23 +86,35 @@ namespace MpLabChat
       login.IsEnabled = true;
 
       logins.IsEnabled = false;
-      os.Visibility = Visibility.Hidden;
+      
     }
     private void nameE_Click(object sender, RoutedEventArgs e)
     {
       //создание нового чата
+      var chatClient = new ChatServiceClient();
+      chatClient.CreatSpisok(name.Text);
+      name.Text = "";
+      
     }
     private void MenuItem_Click(object sender, RoutedEventArgs e)
     {
       //удалить чат из списка
+      if (names.SelectedIndex == -1 || names.SelectedIndex >= names.Items.Count) return;
+      var chatClient = new ChatServiceClient();
+      chatClient.DelSpisok((string)((ListBoxItem)names.Items[names.SelectedIndex]).Content);
     }
     private void enter_Click(object sender, RoutedEventArgs e)
     {
       //передача в чат сообщения
+      var chatClient = new ChatServiceClient();
+      var soobsh = string.Format("{0} - {1}\n{2}\n", DateTime.Now, login.Text, vvod.Text);
+      chatClient.Chat((string)((ListBoxItem)names.Items[names.SelectedIndex]).Content, soobsh);
+      vvod.Text = "";
     }
     private void names_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
       //выбирается некоторый чат
+      pole.Text = "";
     }
   }
 }
